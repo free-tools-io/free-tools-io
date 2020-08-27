364d435b5b8478604c120fd72623ee595eb58ffd
free-tools-io <admin@free-tools.io>
2020-08-27 16:24:47 +0530

    meta info changes

--- password-generator/run.js
+++ password-generator/run.js
@@ -2,11 +2,14 @@ var generatePassword = require('password-generator');
 
 module.exports.run = (query) => {
     return new Promise((resolve, reject) => {
+        if(!query.length)
+            query.length = 12;
+
         if(query.length && query.length.length > 3)
-        query.length = 999;
+            query.length = 999;
 
         return resolve({
-            data: generatePassword(query.length ? query.length : 12),
+            data: generatePassword(query.length),
             status: 'success',
             title: 'Generate Password with length=' + query.length
         });
