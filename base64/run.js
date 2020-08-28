8a73439d6d42ea00dfd7b0898364bf3045258b9f
free-tools-io <admin@free-tools.io>
2020-08-28 08:58:49 +0530

    Base64 - Dynamic form + ValiddatorJS integration

--- base64/run.js
+++ base64/run.js
@@ -1,30 +1,34 @@
+let Validator = require('validatorjs');
+
+let rules = {
+    string: 'required|max:10000',
+    action: 'in:Encode,Decode'
+};
 
 module.exports.run = (req) => {
     return new Promise((resolve, reject) => {
-        if(!req.query.string)
-            return resolve();
-            
-        if(req.query.string.length > 10000)
-            return resolve({
-                data: "Invalid String or length is greater than 10000",
-                status: "error"               
-            });
-    
-        if(req.query.action === 'encode') {
-            let buff = new Buffer(req.query.string);
-            return resolve({
-                data: buff.toString('base64'),
-                status: "success",
-                title: "Base64 encode for '" + req.query.string.substring(0,10) + "...'"
-            });
-        } else if(req.query.action === 'decode') {
-            let buff = new Buffer(req.query.string, 'base64');
-            return resolve({
-                data: buff.toString('ascii'),
-                status: "success",
-                title: "Base64 decode for '" + req.query.string.substring(0,10) + "...'"
-            });
-        } else
-            return resolve();
+        let validation = new Validator(req.query, rules);
+        
+        if(validation.passes()) {
+            if(req.query.action === 'Encode') {
+                let buff = new Buffer(req.query.string);
+                return resolve({
+                    data: buff.toString('base64'),
+                    status: "success",
+                    title: "Base64 encode for '" + req.query.string.substring(0,10) + "...'"
+                });
+            } else if(req.query.action === 'Decode') {
+                let buff = new Buffer(req.query.string, 'base64');
+                return resolve({
+                    data: buff.toString('ascii'),
+                    status: "success",
+                    title: "Base64 decode for '" + req.query.string.substring(0,10) + "...'"
+                });
+            }
+        } else {
+            console.log("VALIDATION ERROR", validation.errors.all());
+        }
+        
+        resolve();
     })
 }
